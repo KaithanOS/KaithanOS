4567a8ffe0af0097686284e4c2919667bb1cf8df
Kailash Gautham <kailash.gautham@gmail.com>
2025-06-15 22:21:45 +0800

    Split main asm into bootloader and kernel

---
+++ src/bootloader/boot.asm
@@ -0,0 +1,171 @@
+org 0x7C00 ; calculate all memory offsets from here -> this is a directive
+bits 16 ; Emit 16 bit code
+
+%define ENDL 0x0D, 0x0A
+
+jmp short start
+nop
+
+bdb_oem: db 'MSWIN4.1'
+bdb_bytes_per_sector: dw 512
+bdb_sectors_per_cluster: db 1
+bdb_reserved_sectors: dw 1
+bdb_fat_count: db 2
+bdb_dir_entries_count: dw 0E0h
+bdb_total_sectors: dw 2880
+bdb_media_descriptor_type: db 0F0h
+bdb_sectors_per_fat: dw 9
+bdb_sectors_per_track: dw 18
+bdb_heads: dw 2
+bdb_hidden_sectors: dd 0
+bdb_large_sector_count: dd 0
+
+ebr_drive_number: db 0
+                  db 0
+ebr_signature: db 29h
+ebr_volume_id: db 12h, 34h, 56h, 78h
+ebr_volume_label: db 'KAITHAN  OS'        ; 11 bytes
+ebr_system_id: db 'FAT12   '
+
+start:
+    jmp main
+
+; Prints string to screen
+; Params - ds:si points to string
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+
+.loop:
+    lodsb           ; loads next character in al
+    or al, al
+    jz .done ; jump if zero flag set
+
+    mov ah, 0x0e
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
+main:
+
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    mov ss, ax
+    mov sp, 0x7C00 ; stack grows downwards
+
+    mov [ebr_drive_number], dl
+
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
+    mov si, msg_hello
+    call puts
+
+    cli
+    hlt
+
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0FFFFh:0
+    hlt
+
+.halt:
+    cli
+    hlt
+
+lba_to_chs:
+
+    push ax
+    push dx
+
+    xor dx, dx
+    div word [bdb_sectors_per_track]
+
+    inc dx
+    mov cx, dx
+
+    xor dx, dx
+    div word [bdb_heads]
+
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+
+disk_read:
+
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx
+    call lba_to_chs
+    pop ax
+
+    mov ah, 02h
+    mov di, 3 ; retry n times
+
+.retry:
+    pusha
+    stc
+    int 13h
+    jnc .done
+
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+
+msg_hello: db 'Hello world!', ENDL, 0
+msg_read_failed: db 'Read from disk failed!', ENDL, 0
+
+times 510-($-$$) db 0 ; $ is memory position of current line, $$ is the current section. $ - $$ = length of program so far.
+; Fill 510 bytes, set the last two bytes as AA55 (this is a convention).
+dw 0AA55h
