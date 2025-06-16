5010beb43b3770a4119331704a7aa46e11fc0a32
Kailash Gautham <kailash.gautham@gmail.com>
2025-06-16 22:03:24 +0800

    Add fat logic to read kernel

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,9 +1,10 @@
-org 0x7C00 ; calculate all memory offsets from here -> this is a directive
+org 0x0 ; calculate all memory offsets from here -> this is a directive
 bits 16 ; Emit 16 bit code
 
 %define ENDL 0x0D, 0x0A
 start:
-    jmp main
+    mov si, msg_hello
+    call puts
 
 ; Prints string to screen
 ; Params - ds:si points to string
@@ -11,6 +12,7 @@ puts:
     ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
     lodsb           ; loads next character in al
@@ -18,34 +20,15 @@ puts:
     jz .done ; jump if zero flag set
 
     mov ah, 0x0e
+    mov bh, 0
     int 0x10
 
     jmp .loop
 
 .done:
+    pop bx
     pop ax
     pop si
     ret
 
-main:
-
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-
-    mov ss, ax
-    mov sp, 0x7C00 ; stack grows downwards
-
-    mov si, msg_hello
-    call puts
-
-    hlt ; this is an instruction
-
-.halt:
-    jmp .halt
-
-msg_hello: db 'Hello world!', ENDL, 0
-
-times 510-($-$$) db 0 ; $ is memory position of current line, $$ is the current section. $ - $$ = length of program so far.
-; Fill 510 bytes, set the last two bytes as AA55 (this is a convention).
-dw 0AA55h
+msg_hello: db 'Hello world from KaithanOS Kernel!', ENDL, 0
